43436d7501187812d114e226d6c2ce67ed7cee5e
JoeLof <joelofranco@hotmail.com>
2022-08-08 16:45:50 -0400

    Lorem

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello World!</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea saepe eum veritatis, non adipisci debitis libero. Debitis tempora amet aliquam corporis nam praesentium sapiente fugit ratione laborum cupiditate, eaque quas?</p>
     </div>
   );
 }
